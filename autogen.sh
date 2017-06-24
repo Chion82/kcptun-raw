8bf8073eff0b348c37e6c2ff0705658be5830def
Chion Tang <tech@chionlab.moe>
2017-06-24 17:24:54 +0800

    Add GNU build system

---
+++ autogen.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+aclocal
+autoconf
+autoheader
+automake --add-missing
---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -rf *.cache
+rm INSTALL Makefile Makefile.in aclocal.m4 compile config.h *.log config.status configure depcomp install-sh missing stamp-h1
+rm -rf src/.deps
+rm config.*
+rm src/Makefile
+rm src/Makefile.in
+rm src/kcpraw_*
