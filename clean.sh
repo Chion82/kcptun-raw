3acbfc3f3c5425c9af0bc02d31d8cc3f19177da7
Chion Tang <tech@chionlab.moe>
2017-06-25 19:29:28 +0800

    fix: clean files

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf *.cache
-rm INSTALL Makefile Makefile.in aclocal.m4 compile config.h *.log config.status configure depcomp install-sh missing stamp-h1
+rm INSTALL Makefile Makefile.in aclocal.m4 compile *.log config.status configure depcomp install-sh missing
 rm -rf src/.deps
 rm src/config.*
 rm src/Makefile
